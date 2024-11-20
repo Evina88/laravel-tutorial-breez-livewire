142dfe2716c8072e5676a1e2e8a12c5dab01512f
Evina88 <evinamous@gmail.com>
2024-11-20 15:41:46 +0200

    create Blade template for chirps and use php artisan make:volt chirps/create --class to create a new a new Livewire component to display a form

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class ChirpController extends Controller
 {
      /**
       * Display a listing of the resource.
       */
-    public function index(): Response
+    public function index(): View
     {
-        return response('Chirps!');
+        return view(
+            'chirps', [
+            //
+            ]
+        );
     }
 }
