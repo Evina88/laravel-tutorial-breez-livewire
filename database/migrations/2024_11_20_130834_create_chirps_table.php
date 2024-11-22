3555c8744d70760d0ab5f5ccc26fa67fabc172ec
Evina88 <evinamous@gmail.com>
2024-11-22 14:48:43 +0200

    add user_id and message columns to the chirps_table migration

--- database/migrations/2024_11_20_130834_create_chirps_table.php
+++ database/migrations/2024_11_20_130834_create_chirps_table.php
@@ -11,10 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('chirps', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        Schema::create(
+            'chirps', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+                $table->string('message');
+                $table->timestamps();
+            }
+        );
     }
 
     /**
